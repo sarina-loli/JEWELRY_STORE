f441119332c57a34d10877ccd0ca4048b6a169f8
sarina-loli <sarina123getu@gmail.com>
2026-07-13 10:16:26 -0700

    Configure PostgreSQL database for Render

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -87,13 +87,11 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
 DATABASES = {
     "default": dj_database_url.config(
         default=os.environ.get("DATABASE_URL")
     )
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
